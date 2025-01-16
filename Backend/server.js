6bc9a4c47cc74675ffc952babf8fb78047c9f124
AKHILESH <akhileshchikatla6@gmail.com>
2025-01-16 22:34:08 +0530

    cors issuse solved

--- Backend/server.js
+++ Backend/server.js
@@ -29,12 +29,13 @@ const port =process.env.PORT;
 mongoose.connect(`${process.env.MONGO_STRING}`);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(
-  cors({
-    origin: `${process.env.FRONTEND_URL}`,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: `${process.env.FRONTEND_URL}`,
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
